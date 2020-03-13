210aeee10de10075c374cea3568bcd234208ff61
wujb <wujb@tranwin.org>
2020-03-13 17:29:12 +0800

    ssr

--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,7 +1,7 @@
  
 import React from 'react';
 const  Home=()=>{
-    return <div>home</div>
+    return <div>homesasdas a</div>
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import  express from "express";
+const  express=require("express");
 import Home from "./container/Home";
 import {renderToString} from 'react-dom/server';
 import React from 'react';
+const content =renderToString(<Home />);
 var app=express();
 
 app.get("/",function(req,res){
-    res.send(renderToString(<Home />))
+    res.send(
+        `
+        <html>
+            <head>
+                <title>ssr</title>
+            </head>
+            <body>
+                ${content}
+            </body>
+        </html>
+        `)
 });
 var server=app.listen(3000);
